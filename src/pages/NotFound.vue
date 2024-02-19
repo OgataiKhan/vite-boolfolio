c39029c2df9691e0ef35479eb3b30167948749ef
OgataiKhan <badellino.kevin@gmail.com>
2024-02-19 04:36:10 +0100

    feat: page not found

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,17 @@
+<script>
+
+export default {
+  name: 'NotFound',
+};
+
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      Page Not Found
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
